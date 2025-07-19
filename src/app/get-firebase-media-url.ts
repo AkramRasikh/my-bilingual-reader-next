168d9ff28c2456a727af8310d2e9325a0423bc3d
Akram Rasikh <akram.rasikh@gmail.com>
2025-07-19 12:42:22 +0100

    swapped url for cloudflare

--- src/app/get-firebase-media-url.ts
+++ src/app/get-firebase-media-url.ts
@@ -1,8 +1,6 @@
 export const getFirebaseVideoURL = (mp3FileName: string, language: string) => {
   const languageParam = `${language}-video%2F`;
   const baseURL = process.env.NEXT_PUBLIC_FIREBASE_ASSETS_URL + languageParam;
-  const firebaseToken = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_ID;
-  const url = `${baseURL}${mp3FileName}.mp4?alt=media&token=${firebaseToken}`;
-
+  const url = `${baseURL}${mp3FileName}.mp4`;
   return url;
 };
