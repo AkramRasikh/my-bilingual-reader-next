69c0110ebbac66259eb9cab27888f6f89df59f93
Akram Rasikh <akram.rasikh@gmail.com>
2025-08-02 19:52:41 +0100

    improved word card

--- src/app/WordCard.tsx
+++ src/app/WordCard.tsx
@@ -5,14 +5,30 @@ import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardTitle } from '@/components/ui/card';
 import useWords from './words/useWords';
 import ReviewSRSToggles from './ReviewSRSToggles';
+import { Label } from '@/components/ui/label';
 
-const WordCard = ({ baseForm, id, definition, ...rest }) => {
+const WordCard = ({
+  baseForm,
+  id,
+  definition,
+  surfaceForm,
+  phonetic,
+  transliteration,
+  ...rest
+}) => {
+  console.log('## transliteration', transliteration);
+
   const [openContentState, setOpenContentState] = useState(false);
   const { addWordToBasket, wordBasketState, updateWordData } = useWords();
 
   const isInBasket = wordBasketState?.some((i) => i?.id === id);
   return (
-    <Card className='w-fit p-3'>
+    <Card
+      className='w-fit p-3'
+      style={{
+        animation: 'fadeIn 0.5s ease-out forwards',
+      }}
+    >
       <div className='flex gap-3'>
         <CardTitle className='m-auto'>{baseForm}</CardTitle>
         <Button>EASY</Button>
@@ -33,17 +49,41 @@ const WordCard = ({ baseForm, id, definition, ...rest }) => {
         </Button>
       </div>
       {openContentState && (
-        <CardContent>
-          <ReviewSRSToggles
-            contentItem={{
-              id,
-              ...rest,
+        <>
+          <hr />
+          <CardContent
+            style={{
+              animation: 'fadeIn 0.5s ease-out forwards',
             }}
-            handleReviewFunc={updateWordData}
-            isVocab
-          />
-        </CardContent>
+          >
+            <div className='flex flex-col gap-3 mb-4'>
+              {surfaceForm && <Label>Surface Form: {surfaceForm}</Label>}
+              {phonetic && <Label>Phonetic: {phonetic}</Label>}
+              {transliteration && (
+                <Label>Transliteration: {transliteration}</Label>
+              )}
+            </div>
+            <ReviewSRSToggles
+              contentItem={{
+                id,
+                ...rest,
+              }}
+              handleReviewFunc={updateWordData}
+              isVocab
+            />
+          </CardContent>
+        </>
       )}
+      <style jsx>{`
+        @keyframes fadeIn {
+          from {
+            opacity: 0;
+          }
+          to {
+            opacity: 1;
+          }
+        }
+      `}</style>
     </Card>
   );
 };
