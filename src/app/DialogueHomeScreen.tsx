360978a58142b153a1ac793065db7d13de2a010c
Akram Rasikh <akram.rasikh@gmail.com>
2025-09-12 15:06:11 +0100

    removed loading

--- src/app/DialogueHomeScreen.tsx
+++ src/app/DialogueHomeScreen.tsx
@@ -14,7 +14,6 @@ const DialogueHomeScreen = () => {
   console.log('## story', story);
 
   const getStoryAPI = async ({ isStory }) => {
-    setLoading(true);
     try {
       setLoading(true);
       const res = await fetch(
