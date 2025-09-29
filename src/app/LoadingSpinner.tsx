aaebced3ffdfd21578cb2ffec138d56d4129bbdb
Akram Rasikh <akram.rasikh@gmail.com>
2025-09-29 12:17:00 +0100

    updated loading spinner

--- src/app/LoadingSpinner.tsx
+++ src/app/LoadingSpinner.tsx
@@ -1,5 +1,5 @@
 const LoadingSpinner = () => (
-  <div className='h-5 w-5 border-2 border-gray-300 border-t-blue-500 rounded-full animate-spin'></div>
+  <div className='h-5 w-5 border-2 border-gray-300 border-t-blue-500 rounded-full animate-spin' />
 );
 
 export default LoadingSpinner;
