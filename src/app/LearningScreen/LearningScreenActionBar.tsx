920b62549df1d0800f5ccd443a0dfdc716a94be3
Akram Rasikh <akram.rasikh@gmail.com>
2025-09-30 19:48:28 +0100

    action buttons updated

--- src/app/LearningScreen/LearningScreenActionBar.tsx
+++ src/app/LearningScreen/LearningScreenActionBar.tsx
@@ -1,3 +1,4 @@
+import { isNumber } from '@/utils/is-number';
 import useLearningScreen from './useLearningScreen';
 import { Button } from '@/components/ui/button';
 
@@ -10,28 +11,39 @@ const LearningScreenActionBar = () => {
     handleStudyFromHere,
     studyFromHereTimeState,
     handleScrollToMasterView,
+    setStudyFromHereTimeState,
   } = useLearningScreen();
 
   const isLooping = loopTranscriptState?.length > 0;
   const scrollToLastReviewed = () =>
     setLatestDueIdState((prev) => ({ ...prev, triggerScroll: true }));
 
+  const studyFromHereTimeStateNumber = isNumber(studyFromHereTimeState);
+
   return (
     <div className='flex flex-col items-start gap-2 my-2 p-2'>
-      <div className='flex gap-2 mx-auto flex-wrap'>
-        <span className='text-sm italic my-auto'>Scroll to: </span>
+      <div className='flex gap-2 flex-wrap'>
+        {!isInReviewMode && (
+          <>
+            <Button onClick={handleStudyFromHere} variant={'outline'}>
+              Study here {studyFromHereTimeState}
+            </Button>
+            {studyFromHereTimeStateNumber && (
+              <Button
+                variant={'destructive'}
+                onClick={() => setStudyFromHereTimeState(null)}
+              >
+                Clear
+              </Button>
+            )}
+            <div className='w-px h-8 my-auto bg-gray-400' />
+          </>
+        )}
         <Button variant={'link'} onClick={handleScrollToMasterView}>
           Current
         </Button>
         <Button variant={'link'} onClick={scrollToLastReviewed}>
-          Last reviewed
-        </Button>
-        <Button
-          variant={'secondary'}
-          disabled={isInReviewMode}
-          onClick={handleStudyFromHere}
-        >
-          Study from here {studyFromHereTimeState}
+          Checkpoint
         </Button>
         {isLooping && !isInReviewMode && (
           <Button variant={'outline'} onClick={handleBulkReviews}>
