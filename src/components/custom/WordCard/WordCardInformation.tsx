4f2a2f1bf3701364f0bfb0977f490a96526d746b
Akram Rasikh <akram.rasikh@gmail.com>
2025-10-13 17:03:53 +0300

    refactor word card:

--- src/components/custom/WordCard/WordCardInformation.tsx
+++ src/components/custom/WordCard/WordCardInformation.tsx
@@ -6,36 +6,30 @@ const WordCardInformation = ({
   phonetic,
   transliteration,
   definition,
-}) => (
-  <AnimationWrapper>
-    <div className={'flex flex-col gap-1 mb-2 flex-wrap items-start'}>
-      {baseForm && (
-        <span className='text-sm text-left font-medium my-auto'>
-          BaseForm: {baseForm}
-        </span>
-      )}
-      {surfaceForm && (
-        <span className='text-sm text-left font-medium'>
-          Surface Form: {surfaceForm}
-        </span>
-      )}
-      {phonetic && (
-        <span className='text-sm text-left font-medium'>
-          Phonetic: {phonetic}
-        </span>
-      )}
-      {transliteration && (
-        <span className='text-sm text-left font-medium'>
-          Transliteration: {transliteration}
-        </span>
-      )}
-      {definition && (
-        <span className='text-sm text-left font-medium'>
-          Definition: {definition}
-        </span>
-      )}
-    </div>
-  </AnimationWrapper>
-);
+}) => {
+  const wordDataArr = [
+    { preText: 'BaseForm', text: baseForm },
+    { preText: 'Surface Form', text: surfaceForm },
+    { preText: 'Phonetic', text: phonetic },
+    { preText: 'Transliteration', text: transliteration },
+    { preText: 'Definition', text: definition },
+  ];
+
+  return (
+    <AnimationWrapper>
+      <div className={'flex flex-col gap-1 mb-2 flex-wrap items-start'}>
+        {wordDataArr.map((wordData) => {
+          return (
+            wordData.text && (
+              <span className='text-sm text-left font-medium'>
+                {wordData.preText}: {wordData.text}
+              </span>
+            )
+          );
+        })}
+      </div>
+    </AnimationWrapper>
+  );
+};
 
 export default WordCardInformation;
