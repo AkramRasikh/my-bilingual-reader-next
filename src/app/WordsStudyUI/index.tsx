2af14047439be48df44c84c09285f6ae92453331
Akram Rasikh <akram.rasikh@gmail.com>
2025-10-16 21:47:14 +0300

    updateWordDataProvider for review

--- src/app/WordsStudyUI/index.tsx
+++ src/app/WordsStudyUI/index.tsx
@@ -2,9 +2,11 @@ import PageContainer from '@/components/custom/PageContainer';
 import { useWordsStudyUIScreen } from './WordsStudyUIProvider';
 import LoadingSpinner from '@/components/custom/LoadingSpinner';
 import WordCard from '@/components/custom/WordCard';
+import useData from '../Providers/useData';
 
 const WordsStudyUI = () => {
   const { formattedWordsStudyState } = useWordsStudyUIScreen();
+  const { updateWordDataProvider } = useData();
 
   const hasWords = formattedWordsStudyState?.length > 0;
   if (!hasWords) {
@@ -24,7 +26,11 @@ const WordsStudyUI = () => {
           {formattedWordsStudyState.map((wordItem, index) => {
             return (
               <div key={wordItem.id}>
-                <WordCard indexNum={index + 1} {...wordItem} />
+                <WordCard
+                  indexNum={index + 1}
+                  {...wordItem}
+                  updateWordData={updateWordDataProvider}
+                />
                 <span>Number of contexts: {wordItem.contextData.length}</span>
               </div>
             );
