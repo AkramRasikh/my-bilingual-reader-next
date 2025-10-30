58a0ff2c85d7b7b3056c1a15995550db6c3db696
Akram Rasikh <akram.rasikh@gmail.com>
2025-10-30 14:42:12 +0300

    useWordsStudyUIinit with hook

--- src/app/WordsStudyUI/WordsStudyUIProvider.tsx
+++ src/app/WordsStudyUI/WordsStudyUIProvider.tsx
@@ -3,10 +3,9 @@ import { isNumber } from '@/utils/is-number';
 import { createContext, useEffect, useRef, useState, useContext } from 'react';
 import useData from '../Providers/useData';
 import useTrackMasterTranscript from '../LearningScreen/hooks/useManageThreeSecondLoop';
-import { underlineWordsInSentence } from '@/utils/underline-words-in-sentences';
 import { useFetchData } from '../Providers/FetchDataProvider';
-import { findAllInstancesOfWordsInSentence } from '@/utils/find-all-instances-of-words-in-sentences';
 import useSentencesProgress from '../sentences/useSentencesProgress';
+import useWordsStudyUIinit from './useWordsStudyUIinit';
 
 const WordsStudyUIContext = createContext(null);
 
@@ -84,111 +83,14 @@ export const WordsStudyUIProvider = ({
 
   const sentencesData = data.sentencesData;
 
-  useEffect(() => {
-    const slicedWords = wordsForReviewMemoized.slice(0, 6);
-    const wordsDataWithContextData = slicedWords.map((item) => {
-      let contextIds = item.contexts;
-
-      const isNumericKeyObject =
-        typeof contextIds === 'object' &&
-        contextIds !== null &&
-        !Array.isArray(contextIds) &&
-        Object.keys(contextIds).every((key) => !isNaN(Number(key)));
-
-      if (isNumericKeyObject) {
-        contextIds = Object.values(contextIds);
-      }
-      const contextData = [];
-
-      for (const contextId of contextIds ?? []) {
-        for (const contentItem of contentState) {
-          const thisContent = contentItem.content;
-
-          const contextSentenceDataIndex = thisContent.findIndex(
-            (contentSentence) => contentSentence.id === contextId,
-          );
-
-          const contextSentenceData = thisContent[contextSentenceDataIndex];
-
-          if (contextSentenceData) {
-            const previousSentence =
-              contextSentenceDataIndex > 0
-                ? {
-                    ...thisContent[contextSentenceDataIndex - 1],
-                    targetLangformatted: underlineWordsInSentence(
-                      thisContent[contextSentenceDataIndex - 1].targetLang,
-                      pureWordsMemoized,
-                    ),
-                  }
-                : null;
-
-            const nextSentence =
-              contextSentenceDataIndex + 1 < thisContent.length
-                ? {
-                    ...thisContent[contextSentenceDataIndex + 1],
-                    targetLangformatted: underlineWordsInSentence(
-                      thisContent[contextSentenceDataIndex + 1].targetLang,
-                      pureWordsMemoized,
-                    ),
-                  }
-                : null;
-
-            const totalObj = {
-              ...contextSentenceData,
-              title: contentItem?.title,
-              generalTopicName: contentItem?.generalTopicName,
-              isMedia: contentItem?.origin === 'youtube',
-              targetLangformatted: underlineWordsInSentence(
-                contextSentenceData.targetLang,
-                pureWordsMemoized,
-              ),
-              contentIndex: contentItem.contentIndex,
-              realStartTime: contentItem?.realStartTime,
-              wordsFromSentence: findAllInstancesOfWordsInSentence(
-                contextSentenceData.targetLang,
-                wordsState,
-              ),
-              ...(previousSentence && { previousSentence }),
-              ...(nextSentence && { nextSentence }),
-            };
-
-            contextData.push(totalObj);
-
-            // Break inner loop since we found a match
-            break;
-          } else {
-            const foundContextIdInAdhocSentences = sentencesData.find(
-              (sentenceItem) => sentenceItem.id === contextId,
-            );
-
-            if (foundContextIdInAdhocSentences) {
-              contextData.push({
-                ...foundContextIdInAdhocSentences,
-                isAdhoc: true,
-                wordsFromSentence: findAllInstancesOfWordsInSentence(
-                  foundContextIdInAdhocSentences.targetLang,
-                  wordsState,
-                ),
-                targetLangformatted: underlineWordsInSentence(
-                  foundContextIdInAdhocSentences.targetLang,
-                  pureWordsMemoized,
-                ),
-              });
-              break;
-            }
-          }
-        }
-      }
-
-      return {
-        ...item,
-        generalTopicName: contextData?.[0]?.generalTopicName,
-        contextData,
-      };
-    });
-
-    setFormattedWordsStudyState(wordsDataWithContextData);
-  }, [wordsForReviewMemoized]);
+  useWordsStudyUIinit({
+    setFormattedWordsStudyState,
+    wordsForReviewMemoized,
+    pureWordsMemoized,
+    contentState,
+    sentencesData,
+    wordsState,
+  });
 
   const realStartTime = selectedContentState?.realStartTime || 0;
 
