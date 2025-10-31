0a036509bd20226b7a35b5c31372cc525f70628f
Akram Rasikh <akram.rasikh@gmail.com>
2025-10-31 16:31:33 +0300

    words due comparison added

--- src/app/LearningScreen/TabContent/LearningScreenTabSelection.tsx
+++ src/app/LearningScreen/TabContent/LearningScreenTabSelection.tsx
@@ -2,14 +2,26 @@ import { TabsList, TabsTrigger } from '@/components/ui/tabs';
 import useLearningScreen from '../useLearningScreen';
 
 const LearningScreenTabs = ({ topicName }) => {
-  const { wordsForSelectedTopic } = useLearningScreen();
+  const {
+    wordsForSelectedTopic,
+    contentMetaMemoized,
+    contentMetaWordMemoized,
+  } = useLearningScreen();
   const thisTopicHasWords = wordsForSelectedTopic?.length > 0;
+
+  const hasUnifiedChapter = contentMetaMemoized?.length === 1;
+
+  const WordsDue = contentMetaWordMemoized[0].length;
+
+  const wordsText = hasUnifiedChapter
+    ? `Words ${WordsDue}/${wordsForSelectedTopic.length}`
+    : 'Words';
 
   return (
     <TabsList>
       <TabsTrigger value='transcript'>Transcript</TabsTrigger>
       <TabsTrigger value='words' disabled={!thisTopicHasWords}>
-        Words {wordsForSelectedTopic.length}
+        {wordsText}
       </TabsTrigger>
       {topicName && <TabsTrigger value='meta'>Meta</TabsTrigger>}
     </TabsList>
