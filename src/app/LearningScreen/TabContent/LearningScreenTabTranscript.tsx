d82690904aa5f6ad107ab6c8f47adb2a95c44747
Akram Rasikh <akram.rasikh@gmail.com>
2025-11-02 19:35:02 +0300

    showing nested word review section

--- src/app/LearningScreen/TabContent/LearningScreenTabTranscript.tsx
+++ src/app/LearningScreen/TabContent/LearningScreenTabTranscript.tsx
@@ -4,8 +4,15 @@ import useLearningScreen from '../useLearningScreen';
 import TranscriptItem from '@/components/custom/TranscriptItem';
 import { TranscriptItemProvider } from '@/components/custom/TranscriptItem/TranscriptItemProvider';
 import { useFetchData } from '@/app/Providers/FetchDataProvider';
+import { useMemo, useState } from 'react';
+import { isDueCheck } from '@/utils/is-due-check';
+import WordCard from '@/components/custom/WordCard';
+import { Switch } from '@/components/ui/switch';
+import { Label } from '@/components/ui/label';
 
 const LearningScreenTabTranscript = () => {
+  const [showNestedRelevantWordsState, setShowNestedRelevantWordsState] =
+    useState(true);
   const {
     formattedTranscriptState,
     threeSecondLoopState,
@@ -32,12 +39,17 @@ const LearningScreenTabTranscript = () => {
     transcriptRef,
     scrollToElState,
     wordsForSelectedTopic,
+    playFromThisContext,
   } = useLearningScreen();
   const {
     languageSelectedState,
     wordsState,
     handleSaveWord,
     handleDeleteWordDataProvider,
+    wordBasketState,
+    updateWordDataProvider,
+    addWordToBasket,
+    addImageDataProvider,
   } = useFetchData();
 
   const learnFormattedTranscript =
@@ -50,12 +62,76 @@ const LearningScreenTabTranscript = () => {
       ? formattedTranscriptState.slice(studyFromHereTimeState)
       : formattedTranscriptState;
 
+  const sentencesForReviewMemoized = useMemo(() => {
+    if (!isInReviewMode || wordsForSelectedTopic?.length === 0) {
+      return [];
+    }
+
+    const sentenceIdsForReview = [];
+
+    learnFormattedTranscript.forEach((transcriptEl) => {
+      if (transcriptEl.dueStatus === 'now') {
+        sentenceIdsForReview.push(transcriptEl.id);
+      }
+    });
+
+    if (sentenceIdsForReview.length === 0) {
+      return [];
+    }
+    const now = new Date();
+
+    return wordsForSelectedTopic.filter((wordItem) => {
+      const firstContext = wordItem.contexts[0];
+      if (
+        sentenceIdsForReview.includes(firstContext) &&
+        isDueCheck(wordItem, now)
+      ) {
+        return true;
+      }
+
+      return false;
+    });
+  }, [learnFormattedTranscript, isInReviewMode, wordsForSelectedTopic]);
+
   const contentClasses = 'p-1 max-h-150 overflow-y-auto';
   return (
     <TabsContent
       value='transcript'
       className={clsx(contentClasses, 'border rounded-lg')}
     >
+      {sentencesForReviewMemoized.length > 0 && (
+        <div className='text-center m-auto p-1.5'>
+          <ul className='flex flex-wrap gap-2.5 m-auto'>
+            <div className='flex gap-2 m-auto'>
+              <Label>Review Relevant Words</Label>
+              <Switch
+                checked={showNestedRelevantWordsState}
+                onCheckedChange={setShowNestedRelevantWordsState}
+              />
+            </div>
+            {sentencesForReviewMemoized.map((word, index) => {
+              const isInBasket = wordBasketState?.some(
+                (i) => i?.id === word.id,
+              );
+
+              return (
+                <li key={word.id} className='mx-auto'>
+                  <WordCard
+                    {...word}
+                    indexNum={index + 1}
+                    updateWordData={updateWordDataProvider}
+                    addWordToBasket={addWordToBasket}
+                    isInBasket={isInBasket}
+                    addImageDataProvider={addImageDataProvider}
+                    playFromThisContext={playFromThisContext}
+                    languageSelectedState={languageSelectedState}
+                  />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
       <ul
         className={clsx(
           'gap-1',
