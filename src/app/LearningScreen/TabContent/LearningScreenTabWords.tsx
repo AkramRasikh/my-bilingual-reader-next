b5771660c44c66d41b533c3b523421f248177405
Akram Rasikh <akram.rasikh@gmail.com>
2025-11-03 13:15:20 +0300

    centrering in ul

--- src/app/LearningScreen/TabContent/LearningScreenTabWords.tsx
+++ src/app/LearningScreen/TabContent/LearningScreenTabWords.tsx
@@ -32,7 +32,7 @@ const LearningScreenTabWords = () => {
   return (
     <TabsContent value='words' className={'p-1 max-h-150 overflow-y-auto'}>
       <div className='text-center m-auto p-1.5'>
-        <ul className='flex flex-wrap gap-2.5'>
+        <ul className='flex flex-wrap gap-2.5 justify-center'>
           {wordsForSelectedTopic.map((word, index) => {
             const isInBasket = wordBasketState?.some((i) => i?.id === word.id);
 
