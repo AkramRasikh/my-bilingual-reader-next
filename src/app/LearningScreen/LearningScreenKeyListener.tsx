3fbd0cbd7b2a00d69b995b5365a382dd36a6693c
Akram Rasikh <akram.rasikh@gmail.com>
2025-11-07 00:15:14 +0300

    can break sentences in 3 second loop

--- src/app/LearningScreen/LearningScreenKeyListener.tsx
+++ src/app/LearningScreen/LearningScreenKeyListener.tsx
@@ -69,6 +69,11 @@ const LearningScreenKeyListener = () => {
 
       if (isInReviewMode && e.key.toLowerCase() === '[') {
         handleJumpToFirstElInReviewTranscript();
+        return;
+      }
+      if (isInReviewMode && e.key.toLowerCase() === ']') {
+        handleJumpToFirstElInReviewTranscript(true);
+        return;
       }
 
       if (shiftKey && isInReviewMode && e.key.toLowerCase() === ')') {
@@ -90,23 +95,35 @@ const LearningScreenKeyListener = () => {
             handleLoopThis3Second();
             return;
           }
-          if (shiftKey && e.key.toLowerCase() === 'arrowright') {
+          if (e.key.toLowerCase() === 'arrowright') {
             handleShiftSnippet(0.5);
             return;
           }
-          if (shiftKey && e.key.toLowerCase() === 'arrowleft') {
+          if (e.key.toLowerCase() === 'arrowleft') {
             handleShiftSnippet(-0.5);
             return;
           }
 
-          if (shiftKey && e.key.toLowerCase() === 'arrowup') {
+          if (e.key.toLowerCase() === 'arrowup') {
             setContractThreeSecondLoopState((prev) => !prev);
             return;
           }
-          if (shiftKey && e.key.toLowerCase() === 'arrowdown') {
+          if (e.key.toLowerCase() === 'arrowdown') {
             // setContractThreeSecondLoopState(true);
             return;
           }
+          // SHIFT + B // not sure if i need this or just remove the ultimate return
+          if (e.shiftKey && e.key.toLowerCase() === 'p') {
+            handleAddMasterToReview();
+            return;
+          }
+
+          // SHIFT + B // not sure if i need this or just remove the ultimate return
+          if (e.shiftKey && e.key.toLowerCase() === 'b') {
+            handleBreakdownMasterSentence();
+            return;
+          }
+
           return;
         }
 
