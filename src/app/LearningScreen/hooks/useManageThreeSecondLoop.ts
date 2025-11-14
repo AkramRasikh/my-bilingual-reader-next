1c43b9f44fb64a40b2e611040e2684a32ae16e3f
Akram Rasikh <akram.rasikh@gmail.com>
2025-11-14 21:42:12 +0000

    extracted logic for threeseoncd loop

--- src/app/LearningScreen/hooks/useManageThreeSecondLoop.ts
+++ src/app/LearningScreen/hooks/useManageThreeSecondLoop.ts
@@ -1,6 +1,53 @@
 import { isNumber } from '@/utils/is-number';
 import { useEffect, useRef } from 'react';
 
+const threeSecondLoopLogic = ({
+  refSeconds,
+  threeSecondLoopState,
+  contractThreeSecondLoopState,
+  formattedTranscriptState,
+  realStartTime,
+  setOverlappingSnippetDataState,
+}) => {
+  const startTime =
+    threeSecondLoopState - (contractThreeSecondLoopState ? 0.75 : 1.5);
+  const endTime =
+    threeSecondLoopState + (contractThreeSecondLoopState ? 0.75 : 1.5);
+
+  const results = [];
+
+  formattedTranscriptState.forEach((item, index) => {
+    const start = item.time + realStartTime;
+    const end =
+      index < formattedTranscriptState.length - 1
+        ? formattedTranscriptState[index + 1].time + realStartTime
+        : start;
+    const duration = end - start;
+
+    const overlapStart = Math.max(start, startTime);
+    const overlapEnd = Math.min(end, endTime);
+
+    if (overlapStart < overlapEnd) {
+      const overlapDuration = overlapEnd - overlapStart;
+      const percentageOverlap = (overlapDuration / duration) * 100;
+      const startPoint = ((overlapStart - start) / duration) * 100;
+
+      results.push({
+        id: item.id,
+        start,
+        end,
+        percentageOverlap: Number(percentageOverlap.toFixed(2)),
+        startPoint: Number(startPoint.toFixed(2)),
+      });
+    }
+  });
+
+  refSeconds.current = threeSecondLoopState;
+  if (results?.length > 0) {
+    setOverlappingSnippetDataState(results);
+  }
+};
+
 const useManageThreeSecondLoop = ({
   threeSecondLoopState,
   contractThreeSecondLoopState,
@@ -22,43 +69,14 @@ const useManageThreeSecondLoop = ({
       isNumber(threeSecondLoopState) &&
       refSeconds?.current !== threeSecondLoopState
     ) {
-      const startTime =
-        threeSecondLoopState - (contractThreeSecondLoopState ? 0.75 : 1.5);
-      const endTime =
-        threeSecondLoopState + (contractThreeSecondLoopState ? 0.75 : 1.5);
-
-      const results = [];
-
-      formattedTranscriptState.forEach((item, index) => {
-        const start = item.time + realStartTime;
-        const end =
-          index < formattedTranscriptState.length - 1
-            ? formattedTranscriptState[index + 1].time + realStartTime
-            : start;
-        const duration = end - start;
-
-        const overlapStart = Math.max(start, startTime);
-        const overlapEnd = Math.min(end, endTime);
-
-        if (overlapStart < overlapEnd) {
-          const overlapDuration = overlapEnd - overlapStart;
-          const percentageOverlap = (overlapDuration / duration) * 100;
-          const startPoint = ((overlapStart - start) / duration) * 100;
-
-          results.push({
-            id: item.id,
-            start,
-            end,
-            percentageOverlap: Number(percentageOverlap.toFixed(2)),
-            startPoint: Number(startPoint.toFixed(2)),
-          });
-        }
+      threeSecondLoopLogic({
+        refSeconds,
+        threeSecondLoopState,
+        contractThreeSecondLoopState,
+        formattedTranscriptState,
+        realStartTime,
+        setOverlappingSnippetDataState,
       });
-
-      refSeconds.current = threeSecondLoopState;
-      if (results?.length > 0) {
-        setOverlappingSnippetDataState(results);
-      }
     }
   }, [
     threeSecondLoopState,
