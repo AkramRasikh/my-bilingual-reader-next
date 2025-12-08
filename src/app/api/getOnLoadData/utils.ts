b6f83394f586d8b5ac81cf2b523f20dd01fb9ff9
Akram Rasikh <akram.rasikh@gmail.com>
2025-12-08 14:19:55 +0000

    remove adhoc reference

--- src/app/api/getOnLoadData/utils.ts
+++ src/app/api/getOnLoadData/utils.ts
@@ -1,6 +1,5 @@
 export const content = 'content';
 export const words = 'words';
-export const adhocSentences = 'adhocSentences';
 export const sentences = 'sentences';
 
 const addContentIndexAndGeneralTopicName = (targetLanguageLoadedContent) =>
