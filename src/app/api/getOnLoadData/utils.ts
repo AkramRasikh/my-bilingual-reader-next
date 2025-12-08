c002970502f483a7991a92626415de19bd5c7d42
Akram Rasikh <akram.rasikh@gmail.com>
2025-12-08 14:12:35 +0000

    removed isFirst hasFollowingVideo

--- src/app/api/getOnLoadData/utils.ts
+++ src/app/api/getOnLoadData/utils.ts
@@ -11,22 +11,10 @@ const formatContentData = (targetLanguageLoadedContent) =>
     const generalTopicName = !stringEndsWithNumber(contentWidget.title)
       ? contentWidget.title
       : getGeneralTopicName(contentWidget.title);
-    const isFirst =
-      contentWidget.title.endsWith('-1') || contentWidget.title.endsWith('-01');
-    const isLastInTotalArr =
-      targetLanguageLoadedContent.length === contentIndex + 1;
-    const hasFollowingVideo = isLastInTotalArr
-      ? false
-      : generalTopicName ===
-        getGeneralTopicName(
-          targetLanguageLoadedContent[contentIndex + 1].title,
-        );
 
     return {
       ...contentWidget,
-      contentIndex: contentIndex,
-      isFirst,
-      hasFollowingVideo,
+      contentIndex,
       generalTopicName,
     };
   });
