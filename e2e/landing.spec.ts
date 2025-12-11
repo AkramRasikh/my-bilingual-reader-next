24ddb6ee14a8b3281c90c046391dbb94fc7b5a57
Akram Rasikh <akram.rasikh@gmail.com>
2025-12-11 11:15:50 +0000

    playwright scaffolding works for chrome

---
+++ e2e/landing.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Yah Dun Kno!/);
+});
+
+test('homepage loads', async ({ page }) => {
+  await page.goto('/');
+
+  // Wait for page to be loaded
+  await page.waitForLoadState('networkidle');
+
+  // Check that the page loaded successfully
+  expect(page.url()).toBe('http://localhost:3000/');
+});
