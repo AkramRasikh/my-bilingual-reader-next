7e30813853a576a32d237ee83cd9b8e6a0e5fc81
Akram Rasikh <akram.rasikh@gmail.com>
2026-03-27 14:53:44 +0000

    small padding changes

--- src/app/LearningScreen/LearningScreenVideoPlayer.tsx
+++ src/app/LearningScreen/LearningScreenVideoPlayer.tsx
@@ -141,7 +141,9 @@ const LearningScreenVideoPlayer = ({ url }: LearningScreenVideoPlayerProps) => {
       </video>
 
       {isPlayingFirstDueSnippet && firstDueSnippet ? (
-        <div className='absolute top-0 left-0 text-white drop-shadow-[0_2px_10px_rgba(0,0,0,0.85)] px-4 py-2 rounded-lg w-full'>
+        <div
+          className="absolute top-0 left-0 text-white drop-shadow-[0_2px_10px_rgba(0,0,0,0.85)] px-4 pt-1 rounded-lg w-full"
+        >
           <SnippetReview
             snippetData={firstDueSnippet}
             dummy
