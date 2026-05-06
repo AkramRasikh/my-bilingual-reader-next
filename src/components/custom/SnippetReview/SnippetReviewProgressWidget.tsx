5df7108e29a30179e5b7e114c8c5c44d246df32a
Akram Rasikh <akram.rasikh@gmail.com>
2026-05-06 16:19:04 +0100

    slightly elevating progresswidget

--- src/components/custom/SnippetReview/SnippetReviewProgressWidget.tsx
+++ src/components/custom/SnippetReview/SnippetReviewProgressWidget.tsx
@@ -10,7 +10,7 @@ const FormattedSentenceSnippetProgressWidget: React.FC<
 > = ({ played }) => (
   <span
     className={clsx(
-      'absolute left-0 -top-0.5 h-0.5 bg-blue-500',
+      'absolute left-0 -top-1 h-0.5 bg-blue-500',
       played ? 'w-full transition-all duration-400' : 'w-0',
     )}
     style={{ borderRadius: '2px', zIndex: 2 }}
